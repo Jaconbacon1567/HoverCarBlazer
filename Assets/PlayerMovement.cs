91f1cbaf9758fe2bf5d3355a4fb32e0c8c7b44a2
finwak12 <finwak@gmail.com>
2015-03-20 17:26:50 -0400

    First Start
    Here are the basic files.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody rb;
+    Vector3 movement;
+    
+    float speed =20;
+
+	// Use this for initialization
+	void Awake () {
+        rb = GetComponent<Rigidbody>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+      
+        rb.velocity = (movement * speed);       
+        
+	}
+}
---
+++ Assets/teleport.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teleport : MonoBehaviour {
+    public Transform exit;
+    public Transform last;
+
+    void OnTriggerEnter(Collider obj)
+    {
+        if (exit == last)
+            return;
+
+        TeleportToExit(obj);
+    }
+
+    void OnTriggerExit()
+    {
+        if (exit == last)
+            last = null;
+    }
+
+    void TeleportToExit(Collider obj)
+    {   
+        last = transform;
+        obj.transform.position = exit.transform.position;
+    }
+}
