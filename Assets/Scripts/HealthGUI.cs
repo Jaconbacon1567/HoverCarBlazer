b2c1fca54d8f728a955e12733da965e02fc14eec
Jaconbacon1567 <anthonyjfeola@gmail.com>
2015-03-29 23:10:05 -0400

    created gui script and menu scene

---
+++ Assets/Scripts/HealthGUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthGUI : MonoBehaviour 
+{
+    public int health;
+    int damage;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	    
+	}
+
+	void OnGUI()
+	{
+
+	}
+
+}
