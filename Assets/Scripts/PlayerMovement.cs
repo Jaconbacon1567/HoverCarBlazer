26936f224315b9bfdab39ef166f1541b4a8a8b58
Jaconbacon1567 <anthonyjfeola@gmail.com>
2015-04-01 16:03:37 -0400

    More movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Xml.Schema;
+
+[System.Serializable]
+public class Boundary
+{
+    public float XMin, XMax, ZMin, ZMax;
+}
 
 public class PlayerMovement : MonoBehaviour {
 
@@ -8,19 +15,20 @@ public class PlayerMovement : MonoBehaviour {
     
     public float speed;
 
+    public Boundary bounds;
+
 	// Use this for initialization
 	void Awake () {
         rb = GetComponent<Rigidbody>();
-	    
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 	    float moveHorizontal = Input.GetAxis("Horizontal");
-	    //float moveVertical = Input.GetAxis("Vertical");// Its here where the movement problem is happening when added to horizontal movement
+	    float moveVertical = Input.GetAxis("Vertical");// Its here where the movement problem is happening when added to horizontal movement
 
-        movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
       
         rb.velocity = (movement * speed);
 	}
@@ -28,9 +36,9 @@ public class PlayerMovement : MonoBehaviour {
     void FixedUpdate()
     {
         rb.position = new Vector3(
-    Mathf.Clamp(rb.position.x, -7f, 7f), //x
+    Mathf.Clamp(rb.position.x, bounds.XMin, bounds.XMax), //x
     0.0f,                                //y
-    Mathf.Clamp(rb.position.z, -2f, 10f) //z
+    Mathf.Clamp(rb.position.z, bounds.ZMin, bounds.ZMax) //z
     );
     }
 }
